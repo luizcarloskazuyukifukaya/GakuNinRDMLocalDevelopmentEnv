b350d78005ffc66ba03e5d7a372003dd4970c53e
Kazuyuki Fukaya <luizcarloskazuyukifukaya@gmail.com>
2024-11-29 02:27:10 +0000

    update

--- apply-bug-fixed.sh
+++ apply-bug-fixed.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cp ./docker-compose.override.yml ./RDM-osf.io/
-#cp ./requirements.txt ./RDM-osf.io/
+cp updates/.docker-compose.env RDM-osf.io/
+cp updates/.docker-compose.osf-web.env RDM-osf.io/
+cp updates/.docker-compose.wb.env RDM-osf.io/
+cp updates/.docker-compose.mfr.env RDM-osf.io/
+cp updates/local.py RDM-osf.io/admin/base/settings/local.py
+cp updates/docker-compose.override.yml ./RDM-osf.io/
---
+++ updates/local.py
@@ -0,0 +1,9 @@
+# session:
+# TODO: Uncomment SESSION_COOKIE_DOMAIN if OSFAdmin and OSF domains are different.then set up a shared domain.
+#   ex. OSFAdmin: admin.shared.domain , OSF: web.shared.domain
+#         website/settings/local.py    OSF_COOKIE_DOMAIN = '.shared.domain'
+#         admin/base/settings/local.py SESSION_COOKIE_DOMAIN = '.shared.domain'
+#SESSION_COOKIE_DOMAIN = '.shared.domain'
+from .defaults import *
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,osf.io').split(',')
