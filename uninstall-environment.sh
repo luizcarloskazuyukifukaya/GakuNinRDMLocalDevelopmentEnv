c4ddca82e2d3347ebab1a5dc1a228f9e52640609
Kazuyuki Fukaya <luizcarloskazuyukifukaya@gmail.com>
2024-12-04 14:06:41 +0000

    update

--- uninstall-environment.sh
+++ uninstall-environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo docker-compose down -v
 
-sudo docker-compose stop -t 0 postgres
-sudo docker-compose rm postgres
-sudo docker volume rm osfio_postgres_data_vol
+#sudo docker-compose stop -t 0 postgres
+#sudo docker-compose rm postgres
+#sudo docker volume rm osfio_postgres_data_vol
 
 # remove all containers
 docker rm -f $(docker ps -aq)
